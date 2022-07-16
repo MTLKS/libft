b2a66f9e62769f28851f568496fb139921640367
Matthew Liew <matthew.liew99@gmail.com>
2022-07-16 17:26:58 +0800

    fix on mac

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: maliew <maliew@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 14:49:52 by maliew            #+#    #+#             */
-/*   Updated: 2022/07/12 19:01:43 by maliew           ###   ########.fr       */
+/*   Updated: 2022/07/16 16:59:15 by maliew           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,8 @@ void	*ft_calloc(size_t count, size_t size)
 	void	*res;
 	size_t	len;
 
-	if (!count || !size)
-		return (NULL);
 	len = count * size;
-	if (count != len / size)
+	if (size != 0 && count != len / size)
 		return (NULL);
 	res = (void *)malloc(len);
 	if (!res)
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,19 +6,19 @@
 /*   By: maliew <maliew@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 01:51:20 by maliew            #+#    #+#             */
-/*   Updated: 2022/07/13 20:10:05 by maliew           ###   ########.fr       */
+/*   Updated: 2022/07/16 17:20:54 by maliew           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	void	*buffer;
 
-	buffer = dest;
-	if (!dest || !src)
+	if (!dest && !src)
 		return (0);
+	buffer = dest;
 	while (n-- > 0)
 	{
 		*((char *) dest) = *((char *) src);
